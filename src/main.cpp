d28b89d4fec16ad367c8032e7355fb5bc702bbd9
HenryHoorizadeh <107705963+HenryHoorizadeh@users.noreply.github.com>
2024-12-15 17:41:37 -0800

    eeeeeEEEEEEEEE

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
-#include "main.h"
 
-/**
- * A callback function for LLEMU's center button.
- *
- * When this callback is fired, it will toggle line 2 of the LCD text between
- * "I was pressed!" and nothing.
- */
+#include "main.h"
+#include "api.h"
+#include "pid.h"
+#include "robot.h"
+#include "auton.h"
+
+
+using namespace pros;
+using namespace std;
+
 void on_center_button() {
 	static bool pressed = false;
 	pressed = !pressed;
@@ -16,78 +19,187 @@ void on_center_button() {
 	}
 }
 
-/**
- * Runs initialization code. This occurs as soon as the program is started.
- *
- * All other competition modes are blocked by initialize; it is recommended
- * to keep execution time for this mode under a few seconds.
- */
 void initialize() {
 	pros::lcd::initialize();
 	pros::lcd::set_text(1, "Hello PROS User!");
 
 	pros::lcd::register_btn1_cb(on_center_button);
 }
-
-/**
- * Runs while the robot is in the disabled state of Field Management System or
- * the VEX Competition Switch, following either autonomous or opcontrol. When
- * the robot is enabled, this task will exit.
- */
 void disabled() {}
 
-/**
- * Runs after initialize(), and before autonomous when connected to the Field
- * Management System or the VEX Competition Switch. This is intended for
- * competition-specific initialization routines, such as an autonomous selector
- * on the LCD.
- *
- * This task will exit when the robot is enabled and autonomous or opcontrol
- * starts.
- */
+void autonomous() {
+	autons1();
+}
 void competition_initialize() {}
 
-/**
- * Runs the user autonomous code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the autonomous
- * mode. Alternatively, this function may be called in initialize or opcontrol
- * for non-competition testing purposes.
- *
- * If the robot is disabled or communications is lost, the autonomous task
- * will be stopped. Re-enabling the robot will restart the task, not re-start it
- * from where it left off.
- */
-void autonomous() {}
 
-/**
- * Runs the operator control code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the operator
- * control mode.
- *
- * If no competition control is connected, this function will run immediately
- * following initialize().
- *
- * If the robot is disabled or communications is lost, the
- * operator control task will be stopped. Re-enabling the robot will restart the
- * task, not resume it from where it left off.
- */
 void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::Motor left_mtr(1);
-	pros::Motor right_mtr(2);
 
-	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
-		int left = master.get_analog(ANALOG_LEFT_Y);
-		int right = master.get_analog(ANALOG_RIGHT_Y);
+bool MogoMechToggle = false;
+bool arcToggle = true;
+bool tankToggle=false;
+bool StakeWingToggle=false;
+double liftAngle=true;
+int time=0;
+bool hooks_Macro = false;
+bool  hooks_Macro_Rev = false;
 
-		left_mtr = left;
-		right_mtr = right;
 
-		pros::delay(20);
-	}
+while (true) {
+
+if(con.get_digital_new_press(E_CONTROLLER_DIGITAL_A)){
+
+ColorSenseIntake(127, true);
+// hooks_Macro = true;
+
+// while (hooks_Macro){
+// 	Intake.move(95);
+// 	Intake_Layer1.move(95);
+// 	if (dis.get() <= 300){
+// 		delay(300);
+// 		Intake.move(-90);
+// 		delay(150);
+// 		Intake.move(0);
+// 	}
+// }
+
 }
+
+//Mogo Mech
+if(con.get_digital_new_press(E_CONTROLLER_DIGITAL_X)){
+	MogoMechToggle = !MogoMechToggle;
+}
+MogoMech.set_value(MogoMechToggle);
+
+//Stakewing toggle
+if (con.get_digital_new_press(E_CONTROLLER_DIGITAL_Y)) {
+            StakeWingToggle = !StakeWingToggle;
+ }
+ StakeWing.set_value(StakeWingToggle);
+
+ //Redirect
+if (con.get_digital(E_CONTROLLER_DIGITAL_L1)){
+	Redirect.move(90);
+	// liftAngle = Redirect.get_position();	
+}
+else if (con.get_digital(E_CONTROLLER_DIGITAL_L2)){
+	Redirect.move(-70);
+	// liftAngle = Redirect.get_position();
+}
+else {
+	// setConstants(LIFT_KP,LIFT_KI,LIFT_KD);
+	Redirect.move(0);
+	// Redirect.move(calPID(liftAngle,Redirect.get_position(),0,0));
+}
+ //yapp 
+//pid tester
+// if(con.get_digital_new_press(E_CONTROLLER_DIGITAL_A)){
+// 	StakeWingToggle = !StakeWingToggle;
+// 	Redirect.move(90);
+// 	StakeWingToggle = !StakeWingToggle;
+// 	// Intake.move(-117);
+// 	// StakeWing.set_value(true);
+// 	// delay(700);
+// 	// Intake.move(127);
+// 	// delay(450);
+// 	// Intake.move(0);
+// 	// Redirect.move(-100);
+// 	// delay(900);
+// 	// StakeWing.set_value(false);
+// 	// //driveArcL(90, 800, 1000);
+// 	//autonomous(); 
+// 	//driveSlow(1000,80);
+// 	//make sure that this works for small and big numbers 
+// 	//driveTurn(180);
+// }
+
+
+
+//Intake
+if (con.get_digital(E_CONTROLLER_DIGITAL_R1)){
+	Intake.move(-127);
+	Intake_Layer1.move(-127);
+	Intake.tare_position();
+	hooks_Macro = false; 
+}
+else if (con.get_digital(E_CONTROLLER_DIGITAL_R2)){
+	Intake.move (127);
+	Intake_Layer1.move(127);
+	Intake.tare_position(); 
+	hooks_Macro = false; 
+}
+// else if(hooks_Macro){
+	
+//  if(Intake.get_position() > 900){
+// 	hooks_Macro_Rev = true;
+//  }
+//  if((hooks_Macro_Rev == true) && (Intake.get_position() < 200)){
+
+// 	hooks_Macro_Rev = false;
+// 	hooks_Macro = false;
+//  } else if(hooks_Macro_Rev){
+// 	delay(1000);
+// 	setConstants(1, 0, 0);
+// 	Intake.move(calPID (150, Intake.get_position(), 0, 0));
+// 	Intake_Layer1.move(calPID (150, Intake.get_position(), 0, 0));
+//  } else{
+// 	setConstants(1, 0, 0);
+// 	Intake.move(calPID (850, Intake.get_position(), 0, 0));
+// 	Intake_Layer1.move(calPID (1000, Intake.get_position(), 0, 0));
+//  }
+
+//}
+else {
+	Intake.move(0);
+	Intake_Layer1.move(0);
+	Intake.tare_position(); 
+	hooks_Macro = false; 
+}
+//chassis drive 
+int power = con.get_analog(ANALOG_LEFT_Y);
+int RX = con.get_analog(ANALOG_RIGHT_X);
+int turn = int(pow(RX, 3)/ 16129);
+int left= power + turn; 
+int right = power - turn;
+
+// if(con.get_digital_new_press(E_CONTROLLER_DIGITAL_B)){
+// 	arcToggle = !arcToggle;
+// 	tankToggle = !tankToggle;
+// }
+
+if (tankToggle){
+	LF.move(con.get_analog(ANALOG_LEFT_Y));
+	LM.move(con.get_analog(ANALOG_LEFT_Y));
+	LB.move(con.get_analog(ANALOG_LEFT_Y));
+	RF.move(con.get_analog(ANALOG_RIGHT_Y));
+	RM.move(con.get_analog(ANALOG_RIGHT_Y));
+	RB.move(con.get_analog(ANALOG_RIGHT_Y));
+}
+
+if (arcToggle) {
+LF.move(left);
+LM.move(left);
+LB.move(left);
+RF.move(right);
+RM.move(right);
+RB.move(right);
+}
+
+if (time % 50 == 0 && time % 100 !=0 && time % 150 !=0){
+    con.print(0,0,"Time:%f       ", float(viewTime));
+} else if (time% 100 == 0 && time % 150 !=0){
+    con.print(1,0,"HeadingError!%f          ", float(imu.get_heading()));
+} else if (time % 150 == 0){
+    con.print(2,0,"Error:%f      ",float(error));
+}
+
+
+
+
+delay(10);
+time += 10;
+
+}
+
+}
+
